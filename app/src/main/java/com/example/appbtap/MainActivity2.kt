08d0ced61b5846671dd160c61b7fe1db22ad0b43
Hoang <doanhoang2509@gmail.com>
2024-05-07 23:35:34 +0700

    T4.2-BaiTap-A45895-DoanMinhHoang

---
+++ app/src/main/java/com/example/appbtap/MainActivity2.kt
@@ -0,0 +1,30 @@
+package com.example.appbtap
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    var context: Context = this
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        var btnGet=findViewById<Button>(R.id.btnGetData)
+        var tvKQ=findViewById<TextView>(R.id.tvKQ)
+        var fn1 = VolleyFn1()
+        btnGet!!.setOnClickListener {
+            fn1.getAllData(context,tvKQ!!)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
